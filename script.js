559027cd498c550020816541901afa1c6c224bbb
louisedom0310-rgb <louisedom0310@gmail.com>
2026-04-13 20:24:36 +0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,34 @@
+function startMic() {
+  document.getElementById("song").play();
+
+  navigator.mediaDevices.getUserMedia({ audio: true })
+    .then(stream => {
+      const audioContext = new AudioContext();
+      const mic = audioContext.createMediaStreamSource(stream);
+      const analyser = audioContext.createAnalyser();
+
+      mic.connect(analyser);
+      analyser.fftSize = 256;
+
+      const dataArray = new Uint8Array(analyser.frequencyBinCount);
+
+      function detectSound() {
+        analyser.getByteFrequencyData(dataArray);
+
+        let volume = dataArray.reduce((a, b) => a + b) / dataArray.length;
+
+        if (volume > 50) {
+          blowOut();
+        }
+
+        requestAnimationFrame(detectSound);
+      }
+
+      detectSound();
+    });
+}
+
+function blowOut() {
+  document.querySelectorAll(".flame").forEach(f => f.style.display = "none");
+  alert("🎉 Enjoy yo 24 hours!");
+}
